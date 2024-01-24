d8fde52d5835bebce3bb22e58e79be25886e4c35
Abglaa <Altymysbaevabdulaziz@gmail.com>
2024-01-24 18:07:26 +0600

    homework4

--- homework4/app.js
+++ homework4/app.js
@@ -1,59 +1,30 @@
-function myReverse(inputStr) {
-    let reversedStr = "";
-    for (let i = inputStr.length - 1; i >= 0; i--) {
-        reversedStr += inputStr[i];
+const myRevers = (reverse) => {
+    let newStr = ''
+    for (let i = reverse.length-1; i>=0; i--){
+        newStr+=reverse[i]
     }
-    return reversedStr;
+    return newStr
 }
-
-let inputString = "123456789";
-let result = myReverse(inputString);
-console.log(result);
-
+console.log(myRevers('ilol'))
 
 //2
-//
-
-function capitalizeString(inputStr) {
-    if (inputStr.length === 0) {
-        return inputStr;
-    }
-
-    let capitalizedStr = inputStr.charAt(0).toUpperCase() + inputStr.slice(1).toLowerCase();
-    return capitalizedStr;
+const capitalize = (string) => {
+    console.log(string[0].toUpperCase() + string.slice(1).toLowerCase())
 }
-
-let name1 = "ЕВГЕНИЙ";
-let result1 = capitalizeString(name1);
-console.log(result1)
-
-let name2 = "иВАНОВ";
-let result2 = capitalizeString(name2);
-console.log(result2);
+capitalize('dfvdfvjmvk')
 
 
 //3
 
-
-function charCount(inputStr, char) {
-    const lowerCaseStr = inputStr.toLowerCase();
-    const lowerCaseChar = char.toLowerCase();
-
-    let count = 0;
-
-    for (let i = 0; i < lowerCaseStr.length; i++) {
-        if (lowerCaseStr[i] === lowerCaseChar) {
-            count++;
+const charCount = (str, str1)=> {
+    const lowerInput = str.toLowerCase()
+    const charInput = str1.toLowerCase()
+    let count = 0
+    for (let i =0;i <lowerInput.length;i++){
+        if (lowerInput[i]===charInput){
+            count++
         }
     }
-
-    return count;
+    return count
 }
-
-let str1 = "Abrakadabra";
-let char1 = "a";
-console.log(charCount(str1, char1));
-
-let str2 = "hello";
-let char2 = "z";
-console.log(charCount(str2, char2));
+console.log(charCount('nfjwenfesjnfwen', 's'))
